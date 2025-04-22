049b54ca48815bf53b34f8ef1335e5e6d4935153
muhyadincumar <muhyadincumar23@gmail.com>
2025-04-22 21:02:10 +0300

    Added Functionlity Todo

--- Todo App/main.js
+++ Todo App/main.js
@@ -1,1 +1,20 @@
     
+    function addTodo(){
+    const inputElement = document.querySelector("input");
+     const value = inputElement.value;
+     if(value === ''){
+        alert('Please inter a Todo');
+        return; // stop here
+     }
+     const todoContainer = document.createElement('div')
+     todoContainer.className = 'flex justify-between items-center';
+     const todoTitle = document.createElement('h4');
+     todoTitle.textContent = value;
+     const doneButton = document.createElement('button');
+     doneButton.className = 'bg-green-500  p-2 rounded-lg';
+     doneButton.textContent = 'Done';
+
+     todoContainer.appendChild(todoTitle);
+     todoContainer.appendChild(doneButton);
+     const alltodoContainer = document.querySelector('#todosContaine');
+    }
